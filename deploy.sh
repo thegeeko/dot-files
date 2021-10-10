c15e26a7873caf2965da1325633d535e8c377418
Abdelhadi <abdelhadims@ilcoud.com>
2021-10-10 04:44:16 +0200

    added debloy script

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [ ! -d .git ]; then
+	echo "ERROR : plz run this script inside the dot files repo" 
+	exit
+fi;
+
+nvimc="$HOME/.config/nvim"
+swayc="$HOME/.config/sway"
+waybarc="$HOME/.config/waybar"
+wofic="$HOME/.config/wofi"
+qtilec="$HOME/.config/qtile"
+fishc="$HOME/.config/fish"
+dunstc="$HOME/.config/dunst"
+alacrittyc="$HOME/.config/alacritty"
+
+xinitrc="$HOME/.xinitrc"
+
+function deploy () {
+	conf=$(basename $1)
+	if [ ! -d $conf ] && [ ! -f $conf ]; then 
+		echo "WARNNING : no $conf config"
+		return
+	else
+		echo "found $conf"
+	fi;
+	
+	echo "linking $conf"
+	ln -s $conf $1
+}
+
+deploy "$nvimc" 
+deploy "$swayc"
+deploy "$waybarc"
+deploy "$wofic"
+deploy "$qtilec"
+deploy "$fishc"
+deploy "$dunstc"
+deploy "$alacrittyc"
+deploy "$xinitrc"
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,56 +2,60 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd 'packadd packer.nvim'
+	fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd 'packadd packer.nvim'
 end
 
 return require('packer').startup({function()
-  -- themes
-  use 'eddyekofo94/gruvbox-flat.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'hoob3rt/lualine.nvim'
+	-- themes
+	use 'eddyekofo94/gruvbox-flat.nvim'
+	use 'kyazdani42/nvim-web-devicons'
+	use 'hoob3rt/lualine.nvim'
 
-  -- language support
-  use 'cdelledonne/vim-cmake'
-  use{
-    'hrsh7th/nvim-cmp',
-    requires={
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      { 'ray-x/cmp-treesitter' },
-      { 'f3fora/cmp-spell' },
-      { 'L3MON4D3/LuaSnip' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'neovim/nvim-lspconfig' },
-    }
-  }
-  use{
-    'glepnir/lspsaga.nvim',
-    requires = {{ 'neovim/nvim-lspconfig' }}
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+	-- language support
+	use 'cdelledonne/vim-cmake'
+	use{
+		'hrsh7th/nvim-cmp',
+		requires={
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'hrsh7th/cmp-nvim-lua' },
+			{ 'ray-x/cmp-treesitter' },
+			{ 'f3fora/cmp-spell' },
+			{ 'L3MON4D3/LuaSnip' },
+			{ 'saadparwaiz1/cmp_luasnip' },
+			{ 'neovim/nvim-lspconfig' },
+		}
+	}
+	use{
+		'glepnir/lspsaga.nvim',
+		requires = {{ 'neovim/nvim-lspconfig' }}
+	}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 
-  -- editor tools
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'terrortylor/nvim-comment'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {{ 'nvim-lua/plenary.nvim' }}
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{ 'nvim-lua/plenary.nvim' }, {'nvim-telescope/telescope-fzy-native.nvim'}}
-  }
+	-- editor tools
+	use 'lukas-reineke/indent-blankline.nvim'
+	use 'terrortylor/nvim-comment'
+	use {
+		'lewis6991/gitsigns.nvim',
+		requires = {{ 'nvim-lua/plenary.nvim' }}
+	}
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {{ 'nvim-lua/plenary.nvim' }, {'nvim-telescope/telescope-fzy-native.nvim'}}
+	}
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {{ 'kyazdani42/nvim-web-devicons' }},
+	}
 
 end,
 config = {
-  display = {
-    open_fn = require('packer.util').float,
-  }
+	display = {
+		open_fn = require('packer.util').float,
+	}
 }})
